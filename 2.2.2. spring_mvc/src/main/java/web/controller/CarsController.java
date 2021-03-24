cfc84a2b721416250183d147d4bb031d65cd4c41
Evgeny Anchutin <exs@bk.ru>
2021-03-24 15:42:23 +0300

    CarService added

--- 2.2.2. spring_mvc/src/main/java/web/controller/CarsController.java
+++ 2.2.2. spring_mvc/src/main/java/web/controller/CarsController.java
@@ -6,8 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import web.model.Car;
+import web.service.CarService;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -15,21 +15,16 @@ import java.util.stream.Collectors;
 @Controller
 public class CarsController {
 
-    private List<Car> cars;
+    private CarService service;
 
     public CarsController() {
-        cars = new ArrayList<>();
-        cars.add(new Car("Lada", 100, "white"));
-        cars.add(new Car("Fiat", 90, "red"));
-        cars.add(new Car("Ford", 105, "black"));
-        cars.add(new Car("Mini", 120, "yellow"));
-        cars.add(new Car("Kamaz", 250, "blue"));
+        service = CarService.getService();
     }
 
     @GetMapping(value = "/cars")
     public String printCars(ModelMap model, @RequestParam(name = "count", required = false) Optional<Integer> count) {
-        int carsCount = count.orElse(cars.size());
-        List<Car> carList = cars.stream().limit(carsCount).collect(Collectors.toList());
+        int carsCount = count.orElse(service.getCarsCount());
+        List<Car> carList = service.getCars().limit(carsCount).collect(Collectors.toList());
         model.addAttribute("cars", carList);
         return "cars";
     }
@@ -37,7 +32,7 @@ public class CarsController {
     @GetMapping(value = "/cars/count")
     @ResponseBody
     public String printCars(ModelMap model) {
-        return "" + cars.size();
+        return "" + service.getCarsCount();
     }
 
 }
